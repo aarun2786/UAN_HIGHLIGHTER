de4f1ba09bd10af25632e43418ce33d4bd0dc5f8
aarun2786 <aarun7796@gmail.com>
2024-01-25 21:45:43 +0530

    we update interface with bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, redirect
+from flask import Flask,render_template, request, redirect,send_file,url_for
 from backend import get_excel_data,pdf_highlight
 app = Flask(__name__)
 
@@ -10,8 +10,9 @@ def home():
         excel =  get_excel_data(EXCEL)
         PDF.save(PDF.filename)
         pdf = PDF.filename
+        pdf_p=  url_for(pdf)
         pdf_highlights = pdf_highlight(excel,pdf)
-        return f"{set(pdf_highlights)}"
+        return render_template("input.html",page_no=set(pdf_highlights,pdf=pdf_p))
     return render_template("home.html")
 
 if __name__ == "__main__":
