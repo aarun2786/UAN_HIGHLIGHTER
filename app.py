3bfcaa51db549b4edfaf102627984264b238f4ec
aarun2786 <aarun7796@gmail.com>
2024-01-28 19:05:26 +0530

    added delete file button

--- app.py
+++ app.py
@@ -3,25 +3,34 @@ from backend import get_excel_data,pdf_highlight
 app = Flask(__name__)
 import time
 import os
+pdf = ""
 @app.route('/',methods=['POST','GET'])
 def home():
- 
+    global pdf
     if request.method == 'POST':
         EXCEL = request.files['excel']
         PDF = request.files['pdf']
+        color = tuple(request.form['color'])
         excel =  get_excel_data(EXCEL)
         PDF.save(PDF.filename)
-        global pdf
         pdf = PDF.filename
-        pdf_highlights = pdf_highlight(excel,pdf)
+        pdf_highlights = pdf_highlight(excel,pdf,color)
+        
         return render_template("input.html",page_no=set(pdf_highlights))
     return render_template("home.html")
 
 @app.route('/download')
 def download_file():
+    global pdf
     filename = pdf
     return send_file(filename, as_attachment=True)
 
+@app.route('/delete')
+def delete_file():
+    global pdf
+    filename = pdf
+    os.remove(filename)
+    return "file delete"
 if __name__ == "__main__":
     app.run(debug=True)
 
--- backend.py
+++ backend.py
@@ -9,8 +9,7 @@ def get_excel_data(file_path):
         data_list.extend((list(row)))
     workbook.close()
     return data_list
-color = (1,0,1)
-def pdf_highlight(excel,pdf):
+def pdf_highlight(excel,pdf,color):
     page_start = 0
     doc = fitz.open(pdf)
     page_num = []
@@ -20,7 +19,7 @@ def pdf_highlight(excel,pdf):
             text = page_no.search_for(uan,quads=True)
             if text:
                 htext =  page_no.add_highlight_annot(text)
-                htext.set_colors(stroke=color)
+                htext.set_colors(stroke=(0,0,1))
                 htext.update(opacity=0.5)
                 page_num.append(i+1)
                 page_start = i
