cceae22aea351b9712f04587223f8c17b71eda53
aarun2786 <150610612+aarun2786@users.noreply.github.com>
2024-01-30 20:09:28 +0530

    change in active sheets

--- backend.py
+++ backend.py
@@ -2,13 +2,14 @@ import openpyxl
 import fitz
 def get_excel_data(file_path):
     workbook = openpyxl.load_workbook(file_path)
-    sheet = workbook['UAN']
+    sheet = workbook.active
     max_row = sheet.max_row
     data_list = []
     for index, row in enumerate(sheet.iter_rows(min_row=1, max_row=max_row, values_only=True), start=1):
         data_list.extend((list(row)))
     workbook.close()
     return data_list
+
 def pdf_highlight(excel,pdf,color):
     page_start = 0
     doc = fitz.open(pdf)
